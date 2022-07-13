057a92dc6eed0c93f3428b01572cd00db6aed899
RexileSH <yasserre@outlook.sa>
2022-07-13 07:19:26 +0300

    master

---
+++ day3/learn1.py
@@ -0,0 +1,13 @@
+print("Welcome to the rollercoaster!")
+height= int(input("What is your height in cm?"))
+age=int(input("enter your age"))
+if height>120:
+    print("you can ride the rollercoaster!")
+    if age <12:
+     print ("your tecket price will be 5$")
+    elif age <= 18:
+        print("your tecket price will be 7$")
+    else :
+     print("your tecket price will be 12$")
+else:
+    print("sorry")
---
+++ day3/learn2.py
@@ -0,0 +1,5 @@
+number=int(input("enter your numbrer"))
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
