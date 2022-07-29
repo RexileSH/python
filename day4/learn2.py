9fefdfd3d0a8a5d4f18acba5dc71173e492c6419
RexileSH <yasserre@outlook.sa>
2022-07-29 22:01:09 +0300

    master

---
+++ day4/learn2.py
@@ -0,0 +1,12 @@
+import random
+from unicodedata import name
+names_string = input("Give me everybody names, seprated by a comma. ")
+names = names_string.split(",")
+num_items = len(names)
+random_choice = random.randint(0, num_items -1) 
+person=names[random_choice]
+print(person)
+ #بطريقة اخرى ل اختيار عشوائي
+person2=random.choice(names)
+print(person2)
+
