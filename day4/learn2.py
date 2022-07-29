98c290ebde5c6dcac6f61c27188ff37a2b310bb1
RexileSH <yasserre@outlook.sa>
2022-07-30 02:29:09 +0300

    master

--- day4/learn2.py
+++ day4/learn2.py
@@ -6,6 +6,7 @@ num_items = len(names)
 random_choice = random.randint(0, num_items -1) 
 person=names[random_choice]
 print(person)
+print(num_items)
  #بطريقة اخرى ل اختيار عشوائي
 person2=random.choice(names)
 print(person2)
---
+++ day4/learn3.py
@@ -0,0 +1,5 @@
+#طريقة جمع المجموعات المختلفة
+boy=["yasser","saeed","ahmed","khaled"]
+girl=["amal","sharah","nor"]
+combine=[boy,girl]
+print(combine)
