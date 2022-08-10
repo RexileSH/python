9b598a86402d9ef436b25b360979e1f81bbe668d
RexileSH <yasserre@outlook.sa>
2022-08-10 19:22:54 +0300

    master

--- day4/learn2.py
+++ day4/learn2.py
@@ -4,6 +4,7 @@ names_string = input("Give me everybody names, seprated by a comma. ")
 names = names_string.split(",")
 num_items = len(names)
 random_choice = random.randint(0, num_items -1) 
+print(random_choice)
 person=names[random_choice]
 print(person)
 print(num_items)
